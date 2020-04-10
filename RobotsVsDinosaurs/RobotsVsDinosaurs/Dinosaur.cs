10ab89fc00cdcb277332eb89248d9d2bfcbaff17
danamurphy48 <danamurphy48@gmail.com>
2020-04-10 13:26:37 -0500

    Added initial project files

---
+++ RobotsVsDinosaurs/RobotsVsDinosaurs/Dinosaur.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsVsDinosaurs
+{
+    class Dinosaur
+    {
+        //member variables
+
+        //constructor
+
+        //member methods
+    }
+}
